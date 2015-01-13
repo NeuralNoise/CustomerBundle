1a1540f644b493dffedb8cf2423ac940521f4255
Brian Wood <legion.the.unforgiven@gmail.com>
2015-01-13 16:31:18 -0700

    Added registerTypeIfNotRegistered to boot

--- TerramarCustomerBundle.php
+++ TerramarCustomerBundle.php
@@ -9,7 +9,23 @@ class TerramarCustomerBundle extends Bundle
 {
     public function boot()
     {
-        Type::addType('enum.terramar.customer.interaction_type', 'Terramar\Bundle\CustomerBundle\DbalType\InteractionTypeEnumType');
-        Type::addType('enum.terramar.customer.customer_status',  'Terramar\Bundle\CustomerBundle\DbalType\CustomerStatusEnumType');
+        $this->registerTypeIfNotRegistered('enum.terramar.customer.interaction_type', 'Terramar\Bundle\CustomerBundle\DbalType\InteractionTypeEnumType');
+        $this->registerTypeIfNotRegistered('enum.terramar.customer.customer_status',  'Terramar\Bundle\CustomerBundle\DbalType\CustomerStatusEnumType');
+    }
+
+    /**
+     * Registers a type with Doctrine DBAL if it is not already registered.
+     *
+     * This is necessary because multiple instantiations of this bundle will
+     * cause an error to be thrown by the DBAL.
+     *
+     * @param string $typeName
+     * @param string $className
+     */
+    private function registerTypeIfNotRegistered($typeName, $className)
+    {
+        if (!(Type::hasType($typeName))) {
+            Type::addType($typeName, $className);
+        }
     }
 }
